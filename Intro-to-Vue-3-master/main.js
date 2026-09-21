5fdee6103df7d4f2b0b0aa66498e382ff2bb67c0
Amory <vuongvtgcs220111.fpt.edu.vn>
2026-09-21 09:43:25 +0700

    Done Lesson 2

--- Intro-to-Vue-3-master/main.js
+++ Intro-to-Vue-3-master/main.js
@@ -1,5 +1,5 @@
 const app = Vue.createApp({
-  data() {
+  data: function() { //ES6 Shorthand: ECMAScript: Là bộ tiêu chuẩn kỹ thuật cho ngôn ngữ lập trình kịch bản, đóng vai trò là chuẩn chung cho JavaScript.  (2015)
     return {
       product: "Socks",
     };
